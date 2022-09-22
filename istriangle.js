7919a06f700ef298f207a9b46925076acbe9e567
swapnaliss <swapnalisbhargude123@gmail.com>
2022-09-22 12:32:51 +0530

    functionality completed

---
+++ istriangle.js
@@ -0,0 +1,26 @@
+const inputs = document.querySelectorAll(".angle-input");
+const isTriangleBtn = document.querySelector("#is-triangle-btn");
+const outputE1 = document.querySelector("#output");
+
+
+ function calculateSumOfAngles(angle1 , angle2 , angle3)
+{
+    const sumOfAngles = angle1 + angle2 + angle3;
+    // console.log(angle1);
+    return sumOfAngles;
+
+}
+
+function isTriangle(){
+    const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value))
+     if (sumOfAngles === 180 ) {
+
+        console.log("yay ! The angles form triangle");
+         
+     } else {
+         console.log ("Oh Oh ! The angles don't  form a triangle ");
+     }   
+    
+     
+}
+isTriangleBtn.addEventListener("click",isTriangle)
